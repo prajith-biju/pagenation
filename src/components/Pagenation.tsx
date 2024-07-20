e3c26ef8ce678d348ccdf97df51232a93ec94982
Prajith Biju <prajithbiju05@gmail.com>
2024-07-20 18:40:34 +0530

    added inital rendering funtionality

--- src/components/Pagenation.tsx
+++ src/components/Pagenation.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { pagenationProps } from "../types";
 import { pagesType } from "../types";
+import { PageNumber } from "./PageNumber";
 
 export const Pagenation: React.FC<pagenationProps> = ({
   count,
@@ -8,21 +9,40 @@ export const Pagenation: React.FC<pagenationProps> = ({
   currentPage,
 }) => {
   const [pages, setPages] = useState<pagesType[]>();
+  const [currentPages, setCurrentPages] = useState<pagesType[]>();
 
-  const renderPages = () => {
-    console.log(count, total, currentPage);
+  const renderPagenation = () => {
+    const star = currentPage - 3;
+    const end = currentPage + count - 5;
+    let tempCurrent: pagesType[] = [];
+    if (pages) {
+      tempCurrent = pages.slice(star, end);
+      const endElems = pages.slice(-2);
+      endElems[0].isActive = endElems[1].isActive = true;
+      tempCurrent = tempCurrent.concat(endElems);
+      tempCurrent[0].isActive =
+        tempCurrent[1].isActive =
+        tempCurrent[2].isActive =
+        tempCurrent[2].current =
+          true;
+    }
+    setCurrentPages(tempCurrent);
   };
 
   useEffect(() => {
-    const temp: any = [];
+    const temp: pagesType[] = [];
     temp.length = total;
 
     for (let i = 0; i < temp.length; i++) {
-      temp[i] = { id: 1, num: i + 1, isActive: false, current: false };
+      temp[i] = { id: i, num: i + 1, isActive: false, current: false };
     }
 
     setPages(temp);
   }, []);
+
+  useEffect(() => {
+    renderPagenation();
+  }, [pages]);
 
   return (
     <div className="w-screen h-screen flex items-center justify-center">
@@ -33,6 +53,17 @@ export const Pagenation: React.FC<pagenationProps> = ({
         <button className="p-[8px]">
           <i className="fas fa-angle-left"></i>
         </button>
+      </div>
+
+      <div className="p-[4px] flex">
+        {currentPages?.map((item: pagesType) => (
+          <PageNumber
+            key={item.id}
+            num={item.num}
+            isActive={item.isActive}
+            current={item.current}
+          />
+        ))}
       </div>
 
       <div className="right-btns p-[20px]">
