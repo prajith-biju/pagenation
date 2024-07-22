cafb64c81437b142ed027dda3297953bee652987
Prajith Biju <prajithbiju05@gmail.com>
2024-07-22 10:16:01 +0530

    add initial pangenation rendering functionality

--- src/components/Pagenation.tsx
+++ src/components/Pagenation.tsx
@@ -15,31 +15,40 @@ export const Pagenation: React.FC<pagenationProps> = ({
   const resetCurrent = () => {
     pages?.map((item: pagesType) => {
       item.current = false;
+      item.isActive = false;
+    });
+  };
+
+  const setActivePages = (pageArray: pagesType[]) => {
+    pageArray.map((item: pagesType) => {
+      item.isActive = true;
+      if (item.num === currentNum) {
+        item.current = true;
+      }
     });
   };
 
   const renderPagenation = () => {
     resetCurrent();
-    const star = currentNum - 3;
-    const end = currentNum + count - 5;
     let tempCurrent: pagesType[] = [];
+    const star = currentNum - 3;
+    const end = currentNum + 2;
     if (pages) {
-      tempCurrent = pages.slice(star, end);
-      const endElems = pages.slice(-2);
-      endElems[0].isActive = endElems[1].isActive = true;
-      tempCurrent = tempCurrent.concat(endElems);
-      tempCurrent[0].isActive =
-        tempCurrent[1].isActive =
-        tempCurrent[2].isActive =
-        tempCurrent[2].current =
-          true;
+      const activePages = pages.slice(star, end);
+      const statPages = pages.slice(0, 2);
+      const endPages = pages.slice(-2);
+      setActivePages(activePages);
+      setActivePages(statPages);
+      setActivePages(endPages);
+      tempCurrent = [...statPages, ...activePages, ...endPages];
+      console.log(tempCurrent);
     }
     setCurrentPages(tempCurrent);
   };
 
   useEffect(() => {
     const temp: pagesType[] = [];
-    temp.length = total;
+    temp.length = total / count;
 
     for (let i = 0; i < temp.length; i++) {
       temp[i] = { id: i, num: i + 1, isActive: false, current: false };
